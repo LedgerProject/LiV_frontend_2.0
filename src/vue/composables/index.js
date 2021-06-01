3c05ab51ed860aa88af545e0dd91046242ec39c6
Semen Loktionov <loktionov.kh@gmail.com>
2021-06-01 10:43:53 +0300

    fixed localization validators

--- src/vue/composables/index.js
+++ src/vue/composables/index.js
@@ -1,3 +1,4 @@
 export { useFormatDate } from './useFormatDate'
 export { useField } from './useField'
 export { useForm } from './useForm'
+export { useValidators } from './useValidators'
--- src/vue/composables/useField.js
+++ src/vue/composables/useField.js
@@ -1,26 +1,15 @@
-import { ref, reactive, watch } from 'vue'
-import { useI18n } from 'vue-i18n'
-
-const not = value => !value
+import { ref, watch } from 'vue'
 
 export function useField (field) {
-  const { t } = useI18n({ useScope: 'global' })
-
   const isValid = ref(true)
   const value = ref(field.value)
   const touched = ref(false)
-  const errors = reactive({})
   const errorMessage = ref('')
 
-  const setFieldErrorMessage = (validatorName, validatorArgs) => {
+  const setFieldErrorMessage = message => {
     if (touched.value) {
-      if (validatorName && validatorArgs) {
-        errorMessage.value = t(
-          `validation.field-error_${validatorName}`,
-          { [validatorName]: validatorArgs },
-        )
-      } else if (validatorName) {
-        errorMessage.value = t(`validation.field-error_${validatorName}`)
+      if (message) {
+        errorMessage.value = message
       } else {
         errorMessage.value = ''
       }
@@ -30,12 +19,11 @@ export function useField (field) {
   const reAssign = val => {
     isValid.value = true
     Object.keys(field.validators ?? {}).forEach(validator => {
-      const isFieldValid = field.validators[validator](val)
-      errors[validator] = not(isFieldValid)
+      const validatorErrorMessage = field.validators[validator](val)
 
-      if (not(isFieldValid)) {
+      if (validatorErrorMessage) {
         isValid.value = false
-        setFieldErrorMessage(validator, field.validatorsArgs?.[validator])
+        setFieldErrorMessage(validatorErrorMessage)
       }
     })
   }
@@ -53,7 +41,6 @@ export function useField (field) {
   return {
     value,
     isValid,
-    errors,
     errorMessage,
     touched,
     blur,
